f490d20aa3b7fc6a1b890288c3f6292d8ee5d0a5
koukouzasg <koukouzasg@hotmail.gr>
2019-03-05 13:44:55 +0200

    fix detecion bug

--- samples/wadrivng/wad.py
+++ samples/wadrivng/wad.py
@@ -521,7 +521,7 @@ def detect(model, dataset_dir, subset):
         r = model.detect([image], verbose=0)[0]
         # Encode image to RLE. Returns a string of multiple lines
         source_id = dataset.image_info[image_id]["id"]
-        rle = mask_to_rle(source_id, r["masks"], r["sources"], r["class_ids"])
+        rle = mask_to_rle(source_id, r["masks"], r["scores"], r["class_ids"])
         submission.append(rle)
         # Save image with masks
         visualize.display_instances(
